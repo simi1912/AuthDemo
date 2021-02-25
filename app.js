397c7340851bf9522ce626577042ad8ead1f3213
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-02-25 12:46:37 +0000

    Added sign up functionality.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Ana",
     resave: false,
@@ -22,6 +23,9 @@ app.use(require("express-session")({
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ROUTES
+//========
+
 app.get("/", function(req, res){
    res.render("home");
 });
@@ -30,6 +34,26 @@ app.get("/secret", function(req, res){
     res.render("secret");
 });
 
+//Auth Routes
+
+//Sign Up Form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//Handling Sign Up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}),
+        req.body.password,function(err, user){
+            if(err){
+                console.log(err);
+                return res.render("register");
+            } 
+            passport.authenticate("local")(req, res, function(){
+               res.redirect("/secret");
+            });
+        });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Serves has started...");
