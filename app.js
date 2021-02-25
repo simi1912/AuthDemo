457f56c5be72ab0ef475e9495eebaf41da3793a1
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-02-25 11:08:18 +0000

    Added configuration for db and passport; Added User model.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    localStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
+var User = require("./models/user");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require("express-session")({
+    secret: "Ana",
+    resave: false,
+    saveUnintialized: false
+}));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
    res.render("home");
